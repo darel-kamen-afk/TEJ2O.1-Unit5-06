331f5b446e19b2c97061913225d384f88f1deaca
Darel.Grace Kamen <darel.grace-kamen@stu.ocsb.ca>
2026-03-25 20:52:07 -0400

    i controlled a motor

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Grace
+ * Created on: March 2026
+ * This program will control a motorrrr 
 */
 
 basic.showString('Hello, World!')
