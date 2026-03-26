cf3c04a3485b16ef433a7c5493cc05fa00af9023
Darel.Grace Kamen <darel.grace-kamen@stu.ocsb.ca>
2026-03-25 20:58:01 -0400

    i controlled a motor

--- main.ts
+++ main.ts
@@ -5,4 +5,30 @@
  * This program will control a motorrrr 
 */
 
-basic.showString('Hello, World!')
+// variables
+let servoNumber1 = robotbit.Servos.S1
+
+
+//setup
+basic.showIcon(IconNames.Happy)
+
+
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(servoNumber1, 0)
+    basic.clearScreen()
+    basic.showString("0")
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber1, 180)
+    basic.clearScreen()
+    basic.showString("180")
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
